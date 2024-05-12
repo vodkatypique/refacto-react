36dd6a86a15ada8357ddb55528e1e45b9d52fec9
SamNal2008 <samyn@sipios.com>
2024-05-12 13:01:51 +0200

    fix: test dependencies and remove enzyme to use testing library

--- src/components/DomainFilter/DomainFilter.test.tsx
+++ src/components/DomainFilter/DomainFilter.test.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import DomainFilter from './DomainFilter.component';
 import { render, screen } from '@testing-library/react';
 
 describe('components', () => {
   describe('DomainFilter', () => {
     it('should allow the user to filter', () => {
-      const wrapper = shallow(<DomainFilter domains={['do']} />);
+      render(<DomainFilter domains={['do']} />);
 
-      expect(wrapper.find('select')).toHaveLength(3);
+      expect(screen.getAllByRole('listbox')).toHaveLength(3);
     })
 
     it('should render', async () => {
